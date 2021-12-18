a113027ba8295dee69bc775d10764868a4f9f4f1
sbarrios93 <sbarrios93@gmail.com>
2021-12-18 17:58:15 +0300

    cleanup

--- image_pipeline.py
+++ image_pipeline.py
@@ -1,30 +1,8 @@
-# %%
-
-import glob
-import json
-import os
-import shutil
-import subprocess
-import xml.etree.ElementTree as ET
-from pathlib import Path
-import time
-import sys
-import os
-import yaml
-
-
-import cv2
-sys.path.append(os.path.dirname(os.getcwd()))
-os.chdir('/Users/seba/local-projects/pedestrians')
-
-
-from src import data_utils, frame_extract, skeleton_pipeline
 from data.jaad.jaad_data import JAAD
-
+from src import data_utils, skeleton_pipeline
 
 # %%
-jaad_dir = os.path.join('data/jaad')
-
+jaad_dir = "data/jaad"
 
 jaad = JAAD(jaad_dir)
 
@@ -38,6 +16,3 @@ pipeline = skeleton_pipeline.SkeletonPipeline(jaad_db)
 pipeline.prepare_images()
 
 # %%
-
-
-
